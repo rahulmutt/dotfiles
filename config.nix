c155ae6f4d059d1573691961d00389a99d20530c
Rahul Muttineni <rahulmutt@gmail.com>
2015-10-31 10:34:41 +0530

    Cleaned up dotfiles; added .spacemacs

--- config.nix
+++
@@ -1,5 +0,0 @@
-{
-    allowBroken = true;
-    allowUnfree = true;
-    extra-binary-caches = http://hydra.nixos.org/ http://hydra.cryp.to;
-}
--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? (import /home/rahulmutt/src/nixpkgs {})}: 
-
-(import ./default.nix) {
-    stdenv            = pkgs.stdenv;
-    haskellngPackages = pkgs.haskellngPackages;
-}
-
